016a24b697ab3ab673ccf2cc6615bd2434e73936
sky_k <sky_karlos@hotmail.com>
2016-10-30 23:01:35 -0500

    login

---
+++ app/src/main/java/com/example/sky_k/cliente/Usuario.java
@@ -0,0 +1,27 @@
+package com.example.sky_k.cliente;
+
+/**
+ * Created by 1130613425 on 21/10/2015.
+ */
+public class Usuario {
+
+    private String nombre;
+    private String mensaje;
+
+    public Usuario(String nombre, String mensaje){
+        this.nombre = nombre;
+        this.mensaje =  mensaje;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+}
