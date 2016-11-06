733b83fb87c3d635b89109b0fb0b802d04cddacd
sky_k <sky_karlos@hotmail.com>
2016-11-05 21:48:35 -0500

    con lista

---
+++ app/src/main/java/com/example/sky_k/cliente/Adaptador.java
@@ -0,0 +1,56 @@
+package com.example.sky_k.cliente;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import Serializable.Post;
+
+/**
+ * Created by sky_k on 05/11/2016.
+ */
+
+public class Adaptador extends RecyclerView.Adapter<Adaptador.PostViewHolder> {
+List<Post> listaPosts;
+
+    public Adaptador(List<Post> listaPosts) {
+        this.listaPosts = listaPosts;
+    }
+
+    @Override
+    public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item,parent,false);
+        PostViewHolder holder= new PostViewHolder(v);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(PostViewHolder holder, int position) {
+
+     holder.img.setImageBitmap(BitmapFactory.decodeByteArray(listaPosts.get(position).imagen,0,listaPosts.get(position).imagen.length));
+     holder.name.setText(listaPosts.get(position).nombreUsuario);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaPosts.size();
+    }
+
+    public static class PostViewHolder extends RecyclerView.ViewHolder{
+        ImageView img;
+        TextView name;
+        public PostViewHolder(View itemView) {
+            super(itemView);
+            img= (ImageView) itemView.findViewById(R.id.img_post);
+            name= (TextView) itemView.findViewById(R.id.name_user_post);
+        }
+    }
+}
--- app/src/main/java/com/example/sky_k/cliente/HomeActivity.java
+++ app/src/main/java/com/example/sky_k/cliente/HomeActivity.java
@@ -2,8 +2,11 @@ package com.example.sky_k.cliente;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Vector;
@@ -12,6 +15,9 @@ import Serializable.Post;
 
 public class HomeActivity extends AppCompatActivity implements Observer{
 private ArrayList<Post> postes;
+    RecyclerView rv;
+    LinearLayoutManager llm;
+    Adaptador ad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,7 @@ private ArrayList<Post> postes;
         postes= new ArrayList<>();
         Comunicacion.getInstance().addObserver(this);
         pedirPostes();
+
     }
 
 
@@ -35,7 +42,18 @@ private ArrayList<Post> postes;
 
            System.out.println("recibidoPosts");
            System.out.println(postes.size());
-           
+           System.out.println(postes.get(0).contenidoPost);
+           runOnUiThread(new Runnable() {
+               @Override
+               public void run() {
+                   rv= (RecyclerView) findViewById(R.id.Lista);
+                   llm= new LinearLayoutManager(HomeActivity.this);
+                   rv.setLayoutManager(llm);
+                   ad=new Adaptador(postes);
+                   rv.setAdapter(ad);
+               }
+           });
+
 
        }
     }
